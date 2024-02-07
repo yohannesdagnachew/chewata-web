cfe2eef127e01c692d3aae0e1bfdd23dba0065e5
John D <yohannesdagnachew2020@gmail.com>
2024-02-07 16:14:00 +0300

    amharic movies promo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
-import promo from './promo.png';
+import promamahric from './promamahric.png';
+import promo2 from './promo2.png';
+import am1 from './am1.png';
+import am2 from './am2.png';
 import './App.css';
+import React, {useEffect} from 'react';
+
+
 
 function App() {
 
@@ -9,15 +15,23 @@ function App() {
     window.location.href = 'https://play.google.com/store/apps/details?id=com.amharicmovies.drama';
   }
 
+  useEffect(() => {
+    
+    setTimeout(() => {
+      window.location.href = 'https://play.google.com/store/apps/details?id=com.amharicmovies.drama';
+    }, 4000);
+  }, []);
+
 
 
   return (
     <div className="App">
       <header className="App-header">
-       <h1>ይህ ቃና ቴሌቪዥን ነው።  አፑን ከፕለይስቶር ያውርዱ። </h1>
-
+       <h1>የአማርኛ ፊልም እና ድራማ።  አፑን ከፕለይስቶር ያውርዱ። </h1>
        <button onClick={downloadHandler} className='download-button'>Download</button>
-       <img src={promo} className="App-log" alt="logo" />
+       <img src={am2} className="mobile-image" alt="logo" />
+       <img src={promamahric} className="App-log am-dis" alt="logo" />
+       <img src={promo2} className="App-logo" alt="logo" />
       </header>
     </div>
   );
