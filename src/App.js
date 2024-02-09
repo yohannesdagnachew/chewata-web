c5a8690c2a25006259057ac4ef14f32b33f11296
John D <yohannesdagnachew2020@gmail.com>
2024-02-09 18:27:26 +0300

    nigeria tv promo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import promo from './promo.png';
 import kanamob from './kanamob.png';
 import am2 from './am2.png';
 import './App.css';
+import nigeria from './nigeria.png';
+import nigeriamob from './nigeriam.jpg';
 import React, {useEffect} from 'react';
 
 
@@ -13,25 +15,25 @@ function App() {
 
 
   const downloadHandler = () => {
-    window.location.href = 'https://play.google.com/store/apps/details?id=com.kanachewata.drama';
+    window.location.href = 'https://play.google.com/store/apps/details?id=com.nigeriatv.live';
   }
 
-  useEffect(() => {
+  // useEffect(() => {
     
-    setTimeout(() => {
-      window.location.href = 'https://play.google.com/store/apps/details?id=com.kanachewata.drama';
-    }, 2000);
-  }, []);
+  //   setTimeout(() => {
+  //     window.location.href = 'https://play.google.com/store/apps/details?id=com.kanachewata.drama';
+  //   }, 2000);
+  // }, []);
 
 
 
   return (
     <div className="App">
       <header className="App-header">
-       <h1>ቃና ቲቪን ከሞባይልዎ ይመልከቱ። አፑን ከፕለይስቶር ያውርዱ። </h1>
+       <h1>Download the APP </h1>
        <button onClick={downloadHandler} className='download-button'>Download</button>
-       <img src={kanamob} className="mobile-image" alt="logo" />
-       <img src={promo} className="App-log am-dis" alt="logo" />
+       <img src={nigeriamob} className="mobile-image" alt="logo" />
+       <img src={nigeria} className="App-log am-dis" alt="logo" />
        <img src={promo2} className="App-logo" alt="logo" />
       </header>
     </div>
