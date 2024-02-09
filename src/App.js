108e1eb93b27e13b5e3184dbc5f4a3db819a1ab7
John D <yohannesdagnachew2020@gmail.com>
2024-02-09 18:10:17 +0300

    kana promo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import promamahric from './promamahric.png';
 import promo2 from './promo2.png';
-import am1 from './am1.png';
+import promo from './promo.png';
+import kanamob from './kanamob.png';
 import am2 from './am2.png';
 import './App.css';
 import React, {useEffect} from 'react';
@@ -12,14 +13,14 @@ function App() {
 
 
   const downloadHandler = () => {
-    window.location.href = 'https://play.google.com/store/apps/details?id=com.amharicmovies.drama';
+    window.location.href = 'https://play.google.com/store/apps/details?id=com.kanachewata.drama';
   }
 
   useEffect(() => {
     
     setTimeout(() => {
-      window.location.href = 'https://play.google.com/store/apps/details?id=com.amharicmovies.drama';
-    }, 4000);
+      window.location.href = 'https://play.google.com/store/apps/details?id=com.kanachewata.drama';
+    }, 2000);
   }, []);
 
 
@@ -27,10 +28,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>የአማርኛ ፊልም እና ድራማ።  አፑን ከፕለይስቶር ያውርዱ። </h1>
+       <h1>ቃና ቲቪን ከሞባይልዎ ይመልከቱ። አፑን ከፕለይስቶር ያውርዱ። </h1>
        <button onClick={downloadHandler} className='download-button'>Download</button>
-       <img src={am2} className="mobile-image" alt="logo" />
-       <img src={promamahric} className="App-log am-dis" alt="logo" />
+       <img src={kanamob} className="mobile-image" alt="logo" />
+       <img src={promo} className="App-log am-dis" alt="logo" />
        <img src={promo2} className="App-logo" alt="logo" />
       </header>
     </div>
